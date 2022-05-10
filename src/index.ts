9602ed709c5a17b23988d1c3ec3093058d50c207
zhangboyu <zhangboyu@hosecloud.com>
2022-05-10 13:40:59 +0800

    fix: import

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+// import { messageCenter } from "event-message-center"
 import { messageCenter } from "event-message-center"
 class TaskQueue {
     constructor(parameters) {
-
+        console.log(messageCenter)
     }
 }
